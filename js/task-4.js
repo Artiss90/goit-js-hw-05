1f53737177bce991fbc36f20f824cf9b953044be
Art <Petrenkoartik@gmail.com>
2020-09-11 23:55:53 +0300

    task-5 set-get

--- js/task-4.js
+++ js/task-4.js
@@ -11,18 +11,36 @@
 // Метод pad должен использовать методы append и prepend
 
 // Write code under this line
+class StringBuilder {
+  constructor(value) {
+    this._value = value;
+  }
+  get value() {
+    return this._value;
+  }
+}
+StringBuilder.prototype.append = function (str) {
+  return (this._value += str);
+};
+StringBuilder.prototype.prepend = function (str) {
+  return (this._value = str + this.value);
+};
+StringBuilder.prototype.pad = function (str) {
+  return this.append(str) + this.prepend(str);
+};
 
 // .........................................
-// console.log(typeof StringBuilder);
+console.log(typeof StringBuilder);
 // 'function'
 
-// const builder = new StringBuilder('.');
+const builder = new StringBuilder('.');
 
-// builder.append('^');
-// console.log(builder.value); // '.^'
+builder.append('^');
+console.log(builder.value); // '.^'
 
-// builder.prepend('^');
-// console.log(builder.value); // '^.^'
+builder.prepend('^');
+console.log(builder.value); // '^.^'
 
-// builder.pad('=');
-// console.log(builder.value); // '=^.^='
+builder.pad('=');
+
+console.log(builder.value); // '=^.^='
